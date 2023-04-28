ffc9d75d4c2c7c40eb3829a58e2f6e0cdda3d816
crayzeerr <fyresbm9@gmail.com>
2023-04-27 21:43:27 -0700

    done

--- Assignments/3/maze.cpp
+++ Assignments/3/maze.cpp
@@ -15,6 +15,14 @@
 
 using namespace std;
 
+/**
+ * Outputs the string representation of the 2D array
+ * in which the maze is stored
+ * 
+ * @param Out 
+ * @param Maze 
+ * @return ostream& 
+ */
 ostream &operator<<(ostream &Out, const Maze &Maze) {
    for (int row = 0; row < Maze.height; ++row) {
       for (int col = 0; col < Maze.width; ++col) {
@@ -30,6 +38,11 @@ ostream &operator<<(ostream &Out, const Maze &Maze) {
 // at the same location as the cpp files
 // # need to load data files from current directory as cpp files
 // set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+/**
+ * Constructs a maze from a text file, using a 2D array
+ * 
+ * @param FileName 
+ */
 Maze::Maze(const string &FileName) {
    ifstream inFile;
    inFile.open(FileName);
@@ -50,30 +63,76 @@ Maze::Maze(const string &FileName) {
    }
 }
 
+/**
+ * Returns exit row
+ * 
+ * @return int 
+ */
 int Maze::getExitRow() const {
    return exitRow;
 }
 
+/**
+ * Returns exit col
+ * 
+ * @return int 
+ */
 int Maze::getExitColumn() const {
    return exitColumn;
 }
 
+/**
+ * Returns if the row and col is clear
+ * 
+ * @param Row 
+ * @param Col 
+ * @return true 
+ * @return false 
+ */
 bool Maze::isClear(int Row, int Col) const {
    return Field[Row][Col] == ' ';
 }
 
+/**
+ * Marks the row and col as the path
+ * 
+ * @param Row 
+ * @param Col 
+ */
 void Maze::markAsPath(int Row, int Col) {
    Field[Row][Col] = '*';
 }
 
+/**
+ * Returns if the row and col is the path
+ * 
+ * @param Row 
+ * @param Col 
+ * @return true 
+ * @return false 
+ */
 bool Maze::isPath(int Row, int Col) const {
    return Field[Row][Col] == '*';
 }
 
+/**
+ * Returns if the row and col is a wall
+ * 
+ * @param Row 
+ * @param Col 
+ * @return true 
+ * @return false 
+ */
 bool Maze::isWall(int Row, int Col) const {
    return Field[Row][Col] == 'x';
 }
 
+/**
+ * Marks the row and col as visited
+ * 
+ * @param Row 
+ * @param Col 
+ */
 void Maze::markAsVisited(int Row, int Col) {
    Field[Row][Col] = '+';
 }
